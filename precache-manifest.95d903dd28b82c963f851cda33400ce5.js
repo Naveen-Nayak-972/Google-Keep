2227c360d06d19aa8149e5e0c2741b0cd1fe2807
Dharma Bhukya Naveen Nayak <84572157+Naveen-Nayak-972@users.noreply.github.com>
2023-04-28 19:55:41 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.95d903dd28b82c963f851cda33400ce5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3575ce19c2b516670147466e03f1564f",
+    "url": "/Google-Keep/index.html"
+  },
+  {
+    "revision": "45bfcaebd042ec0a142f",
+    "url": "/Google-Keep/static/js/2.783a4611.chunk.js"
+  },
+  {
+    "revision": "b3ce505c166b873c79d3",
+    "url": "/Google-Keep/static/js/main.a69c302c.chunk.js"
+  },
+  {
+    "revision": "093d8cad7bb9ef82f281",
+    "url": "/Google-Keep/static/js/runtime-main.184e6946.js"
+  }
+]);
